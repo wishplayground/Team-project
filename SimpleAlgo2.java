c004278091c63e154314d1b4728891c898643f0d
Dakshitha Hasindra <hasindradh@gmail.com>
2023-09-22 13:05:04 +0530

    Complete simple algo 2

---
+++ SimpleAlgo2.java
@@ -0,0 +1,9 @@
+public class SimpleAlgo2{
+    public static void main(String[] args) {
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+    }
+}
